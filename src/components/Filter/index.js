c4287f42ed4913d3779d88a3399a048b2a7a62c8
Cynthia Wu <cynthia21wu@gmail.com>
2021-01-30 15:14:35 -0800

    Adjusted styling

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -5,8 +5,8 @@ function Filter(props) {
     return (
         <form className="form">
             <div>
-                <p>Use to </p>
-                <input name="filter" value={props.filter} type = "text" id="filter" placeholder="Filter your search" onChange={props.handleInputChange}/>
+                <p className="instructions">Start typing a name in the box below to filter results.</p>
+                <input className="input" name="filter" value={props.filter} type = "text" id="filter" placeholder="Filter your search" onChange={props.handleInputChange}/>
             </div>
         </form>
     );
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -4,28 +4,32 @@ import "./style.css";
 function Table(props) {
 
     return (
-        <table className="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Image</th>
-                    <th onClick={props.sort} scope="col">Name</th>
-                    <th scope="col">Phone</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">DOB</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.users?.length && props.users.filter(user => (user.name.first + " " + user.name.last).toLowerCase().includes(props.filter)).map(item => 
-                    <tr key={item.index}>
-                        <img src={item.picture.thumbnail}/>
-                        <td>{item.name.first} {item.name.last}</td>
-                        <td>{item.phone}</td>
-                        <td>{item.email}</td>
-                        <td>{new Date(item.dob.date).toLocaleDateString()}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>
+        <div className="container">
+            <div className="row">
+                <table className="col-lg-12 col-md-6 col-sm-2 table table-striped">
+                    <thead className="text-center">
+                        <tr>
+                            <th scope="col">Image</th>
+                            <th onClick={props.sort} scope="col"><u>Name</u></th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody className="">
+                        {props.users?.length && props.users.filter(user => (user.name.first + " " + user.name.last).toLowerCase().includes(props.filter)).map(item => 
+                            <tr key={item.index} className="text-center">
+                                <img className="py-3" src={item.picture.medium}/>
+                                <td className="align-middle">{item.name.first} {item.name.last}</td>
+                                <td className="align-middle">{item.phone}</td>
+                                <td className="align-middle">{item.email}</td>
+                                <td className="align-middle">{new Date(item.dob.date).toLocaleDateString()}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        </div>
     );
 };
 
